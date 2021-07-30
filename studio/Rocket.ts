5357715a5ea5342609cf629cb6eb794be992d73d
kcr336 <46976285+kcr336@users.noreply.github.com>
2021-07-29 20:30:42 -0500

    final product

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -21,6 +21,29 @@ export class Rocket {
         return sum;
     }
 
-    currentMassKg
+    currentMassKg():number{
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems)
+    }
 
-}
+    canAdd(item: Payload): boolean{
+        return (this.currentMassKg() + item.massKg) <= this.totalCapacityKg;
+    }
+
+    addCargo(cargo: Cargo):boolean{
+        if (this.canAdd(cargo)){
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        return false
+
+        }
+
+    addAstronaut(astronaut: Astronaut): boolean{
+        if (this.canAdd(astronaut)){
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        return false;
+    }
+    }
+
